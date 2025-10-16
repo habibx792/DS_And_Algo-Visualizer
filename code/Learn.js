120f73b3f3608f39b698c3ca6c0a1eb53283283b
Ghulam Habib <ghulamhabib.x@gamil.com>
2025-10-16 10:52:09 +0500

    add contact

--- code/Learn.js
+++ code/Learn.js
@@ -2,6 +2,8 @@ const btnAi = document.querySelector("#btnAi");
 const Ai_Data = document.querySelector("#Ai_Data");
 const input=document.querySelector("#inputText");
 let text = document.createElement("p");
+text.classList.add("text-2xl", "font-bold", "text-white","py-4");
+
 text.innerText = `AI Help is now active! 🤖 \n Data Structures and Algorithms are the heart of computer science, shaping how efficiently we solve problems.
 They enable systems to store, access, and manage data in intelligent ways.
 From search engines to self-driving cars, DSA makes technology faster and smarter.
