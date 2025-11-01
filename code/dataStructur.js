462d630ee424170fed76fcf54f1dd4752392da52
Ghulam Habib <ghulamhabib.x@gamil.com>
2025-11-01 22:34:55 +0500

    fixed

--- code/dataStructur.js
+++ code/dataStructur.js
@@ -2,14 +2,22 @@ document.addEventListener("DOMContentLoaded", () => {
     let textArea = document.querySelector(".heroText");
     let text = 'Visualize How different Data structures look like';
     let index = 0;
+    textArea.classList.add("text-4xl", "text-center");
 
     function appearText() {
         if (index < text.length) {
             textArea.innerText += text[index];
             index++;
-            setTimeout(appearText, 30);
+            setTimeout(appearText, 50);
         }
     }
 
-    appearText();
+    function startTyping() {
+        textArea.innerText = "";
+        index = 0;
+        appearText();
+    }
+    startTyping();
+    setInterval(startTyping, 5000);
+
 });
