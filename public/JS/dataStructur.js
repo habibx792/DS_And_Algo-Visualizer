f563ecca948444ff1bfb0b74c2cd9f5f91797c2b
Ghulam Habib <ghulamhabib.x@gamil.com>
2025-11-23 18:45:29 +0500

    i added ds card

--- public/JS/dataStructur.js
+++ public/JS/dataStructur.js
@@ -1,23 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let textArea = document.querySelector(".heroText");
-    let text = 'Visualize How different Data structures look like';
-    let index = 0;
-    textArea.classList.add("text-4xl", "text-center");
-
-    function appearText() {
-        if (index < text.length) {
-            textArea.innerText += text[index];
-            index++;
-            setTimeout(appearText, 50);
-        }
-    }
-
-    function startTyping() {
-        textArea.innerText = "";
-        index = 0;
-        appearText();
-    }
-    startTyping();
-    setInterval(startTyping, 5000);
+   
 
 });
